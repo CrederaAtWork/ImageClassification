58bde2384c5690817ac32f558a27ca338373d547
Josh Thomas <joshthomas735@gmail.com>
2020-05-12 13:49:26 -0500

    Trying to read model from blob storage

--- AzureFunction/ClassifyImageByUrl/__init__.py
+++ AzureFunction/ClassifyImageByUrl/__init__.py
@@ -8,13 +8,48 @@ from tensorflow.keras.applications import VGG19
 from tensorflow.keras.applications.vgg19 import preprocess_input
 from tensorflow.keras.applications.vgg19 import decode_predictions
 
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+
+import os
 import io
 import requests
 
+connect_str = "DefaultEndpointsProtocol=https;AccountName=sigstorageaccount;AccountKey=feha7zkcJeiJJ0JONZGiYxSzUEbXp2OGec2qpt33LJpid5PyzGq2tVKG+r1wiN37gxz2RJwHaROMoiF0qPcV6w==;EndpointSuffix=core.windows.net"
+local_path = "./modeldata"
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    local_model_name = "classification_model.h5"
+
+    # Create the BlobServiceClient object which will be used to create a container client
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+    container_name = "classification-model"
+    container_client = ContainerClient.from_container_url('https://sigstorageaccount.blob.core.windows.net/classification-model')
+
+    logging.info('Created blob service and container client')
+
+    blobs = container_client.list_blobs()
+    for blob in blobs:
+        logging.info(f'Blob Name: {blob.name}')
+        blob_downloader = container_client.download_blob(blob)
+        blob_file = blob_downloader.readall()
+        logging.info(f'Blob File as Bytes: {blob_file}')
+
+
+    logging.info('Created blob client')
+
+    # Download the blob to a local file
+    # Add 'DOWNLOAD' before the .txt extension so you can see both files in the data directory
+    # download_file_path = os.path.join(local_path, local_model_name)
+    # logging.info("\nDownloading blob to \n\t" + download_file_path)
+
+    # with open(download_file_path, "wb") as download_file:
+    #     download_file.write(blob_client.download_blob().readall())
+
+    # logging.info('Downloaded blob')
+
+    # Read the image url from the request
     img_path = req.params.get('path')
     if not img_path:
         try:
